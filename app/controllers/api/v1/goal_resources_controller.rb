8becd66632a4e327582a30ed1b60b6a712a41533
github username <github email address>
2020-05-14 14:21:17 -0400

    API setup with all routes and methods

--- app/controllers/api/v1/goal_resources_controller.rb
+++ app/controllers/api/v1/goal_resources_controller.rb
@@ -3,8 +3,24 @@ module Api
         class GoalResourcesController < ApplicationController
             def index
                 goal_resources = GoalResource.all
-                render json: { status: 'SUCCESS', message:'Loaded articles', data:goal_resources},status: :ok
+                render json: goal_resources, except: [:created_at, :updated_at], include: [:goal]
             end
+
+            def show
+                goal_resource = GoalResource.find(params[:id])
+                render json: goal_resource, except: [:created_at, :updated_at], include: [:goal]
+            end
+
+            def create
+                goal_resource = GoalResource.new(goal_resource_params)
+                render json: goal_resource, except: [:create_at, :update_at]
+            end
+
+            private
+
+            def goal_resource_params
+                params.require(:goal_resource).permit(:goal_id, :name, :description, :url)
+            end 
         end
     end
 end
--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -3,8 +3,27 @@ module Api
         class GoalsController < ApplicationController
             def index
                 goals = Goal.all
-                render json: { status: 'SUCCESS', message:'Loaded articles', data:goals},status: :ok
+                render json: goals.to_json(:include => {
+                    :tasks => {:only => [:name, :description]},
+                    :goal_resources => {:only => [:name, :description, :url]}
+                }, except: [:created_at, :updated_at])
             end
+
+            def show
+                goal = Goal.find(params[:id])
+                render json: goal, except: [:created_at, :updated_at], include: [:tasks]
+            end
+
+            def create
+                goal = Goal.new(goal_params)
+                render json: goal, except: [:create_at, :update_at]
+            end
+
+            private
+
+            def goal_params
+                params.require(:goal).permit(:user_id, :goal_name, :goal_description, :date)
+            end 
         end
     end
 end
